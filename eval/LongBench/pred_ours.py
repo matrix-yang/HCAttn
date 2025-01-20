d05861ad47be51b359cb2bdbd17eed428e294ef9
ydq <matrix-yangdq@qq.com>
2025-01-20 15:22:04 +0800

    clear mem

--- eval/LongBench/pred_ours.py
+++ eval/LongBench/pred_ours.py
@@ -29,9 +29,10 @@ def parse_args(args=None):
         type=str,
         default=None,
     )
-    parser.add_argument("--task", type=str, help="task name",default="none")
+    parser.add_argument("--task", type=str, help="task name", default="none")
     parser.add_argument("--decoding_simulation_length", type=int, default=0)
     parser.add_argument("--attn_sum", type=float, default=0.5)
+    parser.add_argument("--no_quant", action="store_true")
     return parser.parse_args(args)
 
 
@@ -40,6 +41,7 @@ def build_chat(tokenizer, prompt, model_name):
     if "llama-2" in model_name:
         prompt = f"[INST]{prompt}[/INST]"
     return prompt
+
 
 def post_process(response, model_name):
     if "xgen" in model_name:
@@ -110,7 +112,17 @@ def get_pred(
                 use_cache=True,
             )
             past_key_values = output.past_key_values
-            past_key_values = quanter.quant(past_key_values)
+
+            if not args.no_quant:
+                # print('---------------use quant------------------')
+                past_key_values = quanter.quant(past_key_values)
+                #print('mem use before clear', torch.cuda.memory_allocated())
+                #torch.cuda.empty_cache()
+                #print('mem use after clear',torch.cuda.memory_allocated())
+            else:
+                pass
+                # print('---------------no quant------------------')
+
             if decoding_simulation_length > 0:
                 for idx, input_id in enumerate(
                         input.input_ids[0, simulation_start_idx:]
@@ -121,6 +133,7 @@ def get_pred(
                         use_cache=True,
                     )
                     past_key_values = output.past_key_values
+
             pred_token_idx = output.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
             generated_content = [pred_token_idx.item()]
             for _ in range(max_gen - 1):
@@ -135,7 +148,13 @@ def get_pred(
                 generated_content += [pred_token_idx.item()]
                 if pred_token_idx.item() in eos_token_ids:
                     break
-
+        print('mem use after decoder', torch.cuda.memory_allocated())
+        del past_key_values
+        del outputs
+        del pred_token_idx
+        del input
+        torch.cuda.empty_cache()
+        print('del past_key_values after infer', torch.cuda.memory_allocated())
 
         pred = tokenizer.decode(generated_content, skip_special_tokens=True)
         pred = post_process(pred, model_name)
@@ -159,6 +178,7 @@ def seed_everything(seed):
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
     torch.cuda.manual_seed_all(seed)
+
 
 from spare_attn.solution2.build_model_chat import LLamaChat
 
@@ -207,12 +227,22 @@ if __name__ == "__main__":
         os.makedirs("eval/LongBench/pred")
     if not os.path.exists("eval/LongBench/pred_e"):
         os.makedirs("eval/LongBench/pred_e")
+
+    print('init mem use --', torch.cuda.memory_allocated())
     for dataset in datasets:
         lb_path = '/ms/FM/ydq/kvcache/duo-attention/THUDM/LongBench/LongBench.py'
         data = load_dataset(lb_path, dataset, split="test")
         if not os.path.exists(f"eval/LongBench/pred/{model_name}"):
             os.makedirs(f"eval/LongBench/pred/{model_name}")
-        out_path = f"eval/LongBench/pred/{model_name}/{dataset}-attn_{attn_sum}.jsonl"
+        if not args.no_quant:
+            out_path = f"eval/LongBench/pred/{model_name}/{dataset}-attn_{attn_sum}.jsonl"
+        else:
+            out_path = f"eval/LongBench/pred/{model_name}/{dataset}-attn_{attn_sum}-no_quant.jsonl"
+
+        if os.path.exists(out_path):
+            print(f'{out_path} is exists pass dataset {dataset}')
+            continue
+
         prompt_format = dataset2prompt[dataset]
         max_gen = dataset2maxlen[dataset]
         preds = get_pred(
@@ -228,7 +258,7 @@ if __name__ == "__main__":
             args.decoding_simulation_length,
             quanter=llama_chat.quanter
         )
-        print("{} ues cache radio: {:.5f}".format(dataset,np.mean(llama_chat.radio_bag)))
+        print("{} ues cache radio: {:.5f}".format(dataset, np.mean(llama_chat.radio_bag)))
         with open(out_path, "w", encoding="utf-8") as f:
             for pred in preds:
                 json.dump(pred, f, ensure_ascii=False)
