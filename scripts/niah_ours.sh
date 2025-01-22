4808a612db959df79244fcbd1743f9b4c2b9e321
ydq <matrix-yangdq@qq.com>
2025-01-22 16:19:38 +0800

    our niah

---
+++ scripts/niah_ours.sh
@@ -0,0 +1,40 @@
+cd eval/needle
+mkdir -p logs img results
+
+attn_pattern=$1
+sparsity=$2
+model=$3
+context_lengths_min=$4
+s_len=$5
+pretrained_len=$6
+model_provider=$7
+attn_sum=$8
+quant_path=$9
+
+# cut the last part of the path of the attn_pattern to get the name
+attn_pattern_name=$(echo $attn_pattern | rev | cut -d'/' -f1 | rev)
+
+suffix="${attn_pattern_name}-attn_sum=${attn_sum}_quant32K"
+(
+    python -u needle_in_haystack_ours.py --s_len $s_len \
+        --e_len $pretrained_len \
+        --context_lengths_min $context_lengths_min \
+        --context_lengths_max $pretrained_len \
+        --model_provider $model_provider \
+        --model_name_suffix $suffix \
+        --sparsity $sparsity \
+        --simulation_length 0 \
+        --context_lengths_num_intervals 13 \
+        --document_depth_percent_intervals 10 \
+        --prefilling_chunk_size 32000 \
+        --model_path /ms/FM/ydq/kvcache/${model} \
+        --quant_path $quant_path \
+        --attn_sum $attn_sum \
+        --modify
+
+) 2>&1 | tee logs/eval_${model}_${suffix}.log
+
+python visualize.py \
+    --folder_path "results/${model}_${suffix}/" \
+    --model_name "${model}_attn_${attn_sum}_quant32K" \
+    --pretrained_len $pretrained_len
---
+++ scripts/run_niah_ours.sh
@@ -0,0 +1,23 @@
+model="Llama-3-8B-Instruct-Gradient-1048k"
+model_provider=LLaMA
+context_lengths_min=8000
+pretrained_len=128000
+# NOT USE
+sparsity=1
+attn_pattern="attn_0.5_noquant"
+attn_sum=0.5
+quant_path=/ms/FM/ydq/notebook/duo_attn/quant/no_norm_4bits_8196_32K.npy
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash scripts/niah_ours.sh $attn_pattern $sparsity $model $context_lengths_min 0 $pretrained_len $model_provider $attn_sum $quant_path
+
+
+
+
+#model="Llama-2-7B-32K-Instruct"
+#model_provider=LLaMA
+#context_lengths_min=2000
+#pretrained_len=32000
+#sparsity=1
+#attn_pattern="attn_0.5_noqiant"
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 bash scripts/niah_ours.sh $attn_pattern $sparsity $model $context_lengths_min 0 $pretrained_len $model_provider $attn_sum $quant_path
