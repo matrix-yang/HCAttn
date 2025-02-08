c6841a14f9fcfd75fffeef23dae4ef65dff2f52e
ydq <matrix-yangdq@qq.com>
2025-02-08 16:00:51 +0800

    spare qwen

--- spare_attn/solution2/build_model_chat.py
+++ spare_attn/solution2/build_model_chat.py
@@ -55,7 +55,7 @@ def load_model_and_tokenizer(path):
 
 from spare_attn.solution2.modeify_llama import enable_llama_approx_attention_eval
 from spare_attn.solution2.simulation_quant_k import Quanter
-
+from spare_attn.solution2.modeify_qwen import enable_qwen_approx_attention_eval
 
 class LLamaChat:
     def __init__(self,model_path,attn_sum,quant_path,modify=True,dims=4):
@@ -155,3 +155,79 @@ class LLamaChat:
                     past_key_values = output.past_key_values
                     attn_list.append(output.attentions)
         return attns,attn_list
+
+class QwenChat(LLamaChat):
+
+    def __init__(self,model_path,attn_sum,quant_path,modify=True,dims=4):
+        seed_everything(42)
+        device_list = [0]
+        #model_path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7b-chat-hf'
+        # llama_model2path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7B-32K-Instruct'
+        self.model_path=model_path
+        self.radio_bag=[]
+        model, tokenizer, eos_token_ids = load_model_and_tokenizer(model_path)
+        if modify:
+            enable_qwen_approx_attention_eval(model,attn_sum=attn_sum,radio_bag=self.radio_bag)
+        self.model = to_device(model, device_list, enable_tp=True)
+        self.tokenizer = tokenizer
+        self.eos_token_ids = eos_token_ids
+        if quant_path:
+            self.quanter = Quanter(quant_path,dims)
+            self.is_quant=True
+        else:
+            self.is_quant = False
+
+    def chat(self, prompt, max_gen=2, decoding_simulation_length=1):
+        #prompt = build_chat(prompt)
+        messages = [
+            {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+        text = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        input = self.tokenizer([text], return_tensors="pt").to(self.model.device)
+        simulation_start_idx = input.input_ids.shape[-1] - decoding_simulation_length
+        with torch.no_grad():
+            output = self.model(
+                input_ids=input.input_ids[:, :simulation_start_idx],
+                past_key_values=None,
+                use_cache=True,
+            )
+            past_key_values = output.past_key_values
+            # 压缩
+            # s = time.time()
+            if self.is_quant:
+                past_key_values=self.quanter.quant(past_key_values)
+            # e = time.time()
+            # print('----------',e-s)
+            if decoding_simulation_length > 0:
+                for idx, input_id in enumerate(input.input_ids[0, simulation_start_idx:]):
+                    output = self.model(
+                        input_ids=input_id.unsqueeze(0).unsqueeze(0),
+                        past_key_values=past_key_values,
+                        use_cache=True,
+                    )
+                    past_key_values = output.past_key_values
+            logit = output.logits[:, -1, :]
+            pred_token_idx = logit.argmax(dim=-1).unsqueeze(1)
+            generated_content = [pred_token_idx.item()]
+            generated_logits = [logit.detach().cpu().numpy()]
+            for _ in range(max_gen - 1):
+                outputs = self.model(
+                    input_ids=pred_token_idx,
+                    past_key_values=past_key_values,
+                    use_cache=True,
+                )
+                past_key_values = outputs.past_key_values
+                logit = outputs.logits[:, -1, :]
+                pred_token_idx = logit.argmax(dim=-1).unsqueeze(1)
+                generated_content += [pred_token_idx.item()]
+                generated_logits += [logit.detach().cpu().numpy()]
+                if pred_token_idx.item() in self.eos_token_ids:
+                    break
+
+        pred = self.tokenizer.decode(generated_content, skip_special_tokens=True)
+        return pred, generated_logits
--- spare_attn/solution2/modeify_llama.py
+++ spare_attn/solution2/modeify_llama.py
@@ -18,88 +18,7 @@ from transformers.models.llama.modeling_llama import (
     BaseModelOutputWithPast,
 )
 from flash_attn import flash_attn_func, flash_attn_with_kvcache
-
-
-def decode_token(query_states_ori, key_states_ori, value_states_ori, sum_value, radio_bag):
-    if sum_value >= 1:
-        return decode_token_ori(query_states_ori, key_states_ori, value_states_ori)
-    #print('----ori', query_states_ori.shape, key_states_ori.shape)
-    query_states = query_states_ori.transpose(1, 2).squeeze(0)
-    key_states = key_states_ori.transpose(1, 2).squeeze(0)
-    value_states = value_states_ori.transpose(1, 2).squeeze(0)
-    # bsz 1 heads 32 seqlen 128
-    # print('----------------',query_states.device,query_states.shape,query_states.device,key_states.shape)
-    q_heads = query_states.shape[0]
-    k_heads = key_states.shape[0]
-    if q_heads != k_heads:
-        # key_states=key_states.repeat(q_heads,1,1)
-        value_states = value_states.repeat(q_heads, 1, 1)
-    attention_scores = torch.matmul(query_states, key_states.transpose(-1, -2)) * 0.088388
-    #print('----------------', query_states.shape, key_states.shape, attention_scores.shape)
-    # 32 1000
-    attention_probs = torch.softmax(attention_scores, dim=-1, dtype=torch.float32).to(query_states.dtype)
-    # print('attention_probs',attention_probs.shape)
-    # 获取降序排序的索引
-    sorted_indices = torch.argsort(attention_probs, descending=True, dim=-1)
-    # print(sorted_indices.shape)
-    # search_index_ls = [16, 32, 64, 96, 128, 256, 384, 512, 768, attention_probs.shape[-1]]
-    step = int(attention_probs.shape[-1] / 100)
-    search_index_ls = np.arange(1, attention_probs.shape[-1], step)
-    search_index_ls = np.append(search_index_ls, attention_probs.shape[-1])
-    for i in search_index_ls:
-        result = torch.gather(attention_probs, dim=-1, index=sorted_indices[:, :, :i])
-        # print(result.shape,result.sum(-1))
-        #print('----------', sorted_indices.shape, result.shape)
-        if all(result.sum(-1) > sum_value):
-            break
-    selected_indices = sorted_indices[:, :, :i]
-    # print('selected_indices',selected_indices)
-
-    selected_indices = selected_indices.sort(dim=-1).values
-    # print('selected_indices sort',selected_indices)
-    # print('radio',i,key_states.shape[-2],i/key_states.shape[-2])
-    radio_bag.append(i / key_states.shape[-2])
-    # 按原始顺序去除这些索引
-    # print(len(selected_indices),selected_indices)
-    # print(selected_indices.shape)
-    # print(value_states.shape)
-    attn_weights = torch.gather(attention_probs, dim=-1, index=selected_indices)
-    # print('attn_weights',attn_weights.shape)
-    v_index = selected_indices.squeeze(1).unsqueeze(-1).expand(-1, -1, 128)
-    # print('v_index',v_index.shape)
-    select_value_states = torch.gather(value_states, dim=-2, index=v_index)
-    # print(attn_weights.shape,value_states.shape)
-    # print('select_value_states',v_index.shape)
-    # print(select_value_states.shape,select_value_states[1,255]==value_states[1,v_index[1,255,0]])
-    attn_output = torch.matmul(attn_weights, select_value_states)
-    attn_output = attn_output.unsqueeze(0).transpose(1, 2)
-    # print(attn_output.shape)
-    return attn_output
-
-
-def decode_token_ori(query_states, key_states, value_states):
-    # query = query_states.transpose(1, 2)
-    # key = key_states.transpose(1, 2)
-    # value = value_states.transpose(1, 2)
-    # # print(query_states.shape,key_states.shape)
-    # # bsz 1 heads 32 seqlen 128
-    #
-    # attn_weights = torch.matmul(query, key.transpose(2, 3))
-    # attn_weights /= 11.3137
-    #
-    # # 32 1000
-    # attn_weights = torch.nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-    # attn_output = torch.matmul(attn_weights, value)
-    # attn_output = attn_output.transpose(1, 2).contiguous()
-
-    attn_output = flash_attn_func(
-        query_states,
-        key_states,
-        value_states,
-        causal=True,
-        dropout_p=0.0,
-    )
-    return attn_output
+from spare_attn.solution2.spare_decode import decode_token, decode_token_ori
 
 
 def enable_llama_approx_attention_eval(
@@ -186,7 +105,7 @@ def llama_approx_attention_forward(
         value_states = torch.cat([past_key_value[1].transpose(1, 2), value_states], dim=1)
         # torch.Size([1, 1344, 32, 128]) torch.Size([1, 1, 32, 128])
         # print(key_states.shape,query_states.shape)
-    #decode
+    # decode
     if query_states.shape[1] == 1:
         attn_output = decode_token(query_states, key_states, value_states, sum_value=attn_sum, radio_bag=radio_bag)
         # attn_output = flash_attn_func(
