9fb3f7cc746150f121f55677be74a9c635a8a66e
ydq <matrix-yangdq@qq.com>
2025-02-12 15:03:58 +0800

    low mem tova longbench

--- spare_attn/solution2/build_model_chat.py
+++ spare_attn/solution2/build_model_chat.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('/nfs/hw-data/ms/FM/ydq/kvcache/duo-attention/')
 import torch
 import json
@@ -53,35 +54,40 @@ def load_model_and_tokenizer(path):
     model = model.eval()
     return model, tokenizer, eos_token_ids
 
+
 from spare_attn.solution2.simulation_quant_k import Quanter
+
+
 class LLamaChat:
-    def __init__(self,model_path,attn_sum,quant_path,modify,dims=4):
+    def __init__(self, model_path, attn_sum, quant_path, modify, dims=4):
         seed_everything(42)
         device_list = [0]
-        #model_path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7b-chat-hf'
+        # model_path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7b-chat-hf'
         # llama_model2path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7B-32K-Instruct'
-        self.model_path=model_path
-        self.radio_bag=[]
-        self.modify=modify
+        self.model_path = model_path
+        self.radio_bag = []
+        self.modify = modify
         model, tokenizer, eos_token_ids = load_model_and_tokenizer(model_path)
-        #TOVA需要
-        self.init_past_key_values=None
-        if modify=="ours":
+        # TOVA需要
+        self.init_past_key_values = None
+        if modify == "ours":
             from spare_attn.solution2.modeify_llama import enable_llama_approx_attention_eval
-            enable_llama_approx_attention_eval(model,attn_sum=attn_sum,radio_bag=self.radio_bag)
-        elif modify=="sllm":
+            enable_llama_approx_attention_eval(model, attn_sum=attn_sum, radio_bag=self.radio_bag)
+        elif modify == "sllm":
             from spare_attn.solution2.sllm_modeify_llama import enable_llama_approx_attention_eval
             enable_llama_approx_attention_eval(model, attn_sum=attn_sum, radio_bag=self.radio_bag)
-        elif modify=="h2o":
+        elif modify == "h2o":
             from spare_attn.solution2.h2o_modeify_llama import enable_llama_approx_attention_eval
             enable_llama_approx_attention_eval(model, attn_sum=attn_sum, radio_bag=self.radio_bag)
-        elif modify=="tova":
-            from spare_attn.solution2.TOVA import TOVACache, enable_tova_caching
-            enable_tova_caching(model)
-            # cache_size meaning min size
-            self.init_past_key_values =TOVACache(cache_size=128,radio=0.5)
+        elif modify == "tova":
+            # from spare_attn.solution2.TOVA import TOVACache, enable_tova_caching
+            # enable_tova_caching(model)
+            # # cache_size meaning min size
+            # self.init_past_key_values =TOVACache(cache_size=128,radio=0.5)
+            from spare_attn.solution2.tova_modeify_llama import enable_llama_approx_attention_eval
+            enable_llama_approx_attention_eval(model, attn_sum=attn_sum, radio_bag=self.radio_bag)
         else:
-            print("-"*50)
+            print("-" * 50)
             print("this test dont modify attn fwd")
 
         self.model = to_device(model, device_list, enable_tp=True)
@@ -89,17 +95,16 @@ class LLamaChat:
         self.eos_token_ids = eos_token_ids
 
         if quant_path:
-            self.quanter = Quanter(quant_path,dims)
-            self.is_quant=True
+            self.quanter = Quanter(quant_path, dims)
+            self.is_quant = True
         else:
             self.quanter = None
             self.is_quant = False
 
     def chat(self, prompt, max_gen=2, decoding_simulation_length=1):
-        #prompt = build_chat(prompt)
+        # prompt = build_chat(prompt)
         input = self.tokenizer(prompt, truncation=False, return_tensors="pt").to("cuda")
         simulation_start_idx = input.input_ids.shape[-1] - decoding_simulation_length
-
 
         with torch.no_grad():
             output = self.model(
@@ -111,7 +116,7 @@ class LLamaChat:
             # 压缩
             # s = time.time()
             if self.is_quant:
-                past_key_values=self.quanter.quant(past_key_values)
+                past_key_values = self.quanter.quant(past_key_values)
             # e = time.time()
             # print('----------',e-s)
             if decoding_simulation_length > 0:
@@ -144,7 +149,7 @@ class LLamaChat:
         return pred, generated_logits
 
     def attn_analysis(self, prompt, decoding_simulation_length=1):
-        #prompt = build_chat(prompt)
+        # prompt = build_chat(prompt)
         input = self.tokenizer(prompt, truncation=False, return_tensors="pt").to("cuda")
         simulation_start_idx = input.input_ids.shape[-1] - decoding_simulation_length
         with torch.no_grad():
@@ -156,14 +161,14 @@ class LLamaChat:
             )
             past_key_values = output.past_key_values
 
-            attns=output.attentions
+            attns = output.attentions
             # 压缩
             # s = time.time()
             if self.is_quant:
-                past_key_values=self.quanter.quant(past_key_values)
+                past_key_values = self.quanter.quant(past_key_values)
             # e = time.time()
             # print('----------',e-s)
-            attn_list=[]
+            attn_list = []
             if decoding_simulation_length > 0:
                 for idx, input_id in enumerate(input.input_ids[0, simulation_start_idx:]):
                     output = self.model(
@@ -174,32 +179,33 @@ class LLamaChat:
                     )
                     past_key_values = output.past_key_values
                     attn_list.append(output.attentions)
-        return attns,attn_list
+        return attns, attn_list
+
 
 class QwenChat(LLamaChat):
 
-    def __init__(self,model_path,attn_sum,quant_path,modify=True,dims=4):
+    def __init__(self, model_path, attn_sum, quant_path, modify=True, dims=4):
         seed_everything(42)
         device_list = [0]
-        #model_path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7b-chat-hf'
+        # model_path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7b-chat-hf'
         # llama_model2path='/nfs/hw-data/ms/FM/ydq/kvcache/Llama-2-7B-32K-Instruct'
-        self.model_path=model_path
-        self.radio_bag=[]
+        self.model_path = model_path
+        self.radio_bag = []
         model, tokenizer, eos_token_ids = load_model_and_tokenizer(model_path)
         if modify:
             from spare_attn.solution2.modeify_qwen import enable_qwen_approx_attention_eval
-            enable_qwen_approx_attention_eval(model,attn_sum=attn_sum,radio_bag=self.radio_bag)
+            enable_qwen_approx_attention_eval(model, attn_sum=attn_sum, radio_bag=self.radio_bag)
         self.model = to_device(model, device_list, enable_tp=True)
         self.tokenizer = tokenizer
         self.eos_token_ids = eos_token_ids
         if quant_path:
-            self.quanter = Quanter(quant_path,dims)
-            self.is_quant=True
+            self.quanter = Quanter(quant_path, dims)
+            self.is_quant = True
         else:
             self.is_quant = False
 
     def chat(self, prompt, max_gen=2, decoding_simulation_length=1):
-        #prompt = build_chat(prompt)
+        # prompt = build_chat(prompt)
         messages = [
             {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
             {"role": "user", "content": prompt}
@@ -221,7 +227,7 @@ class QwenChat(LLamaChat):
             # 压缩
             # s = time.time()
             if self.is_quant:
-                past_key_values=self.quanter.quant(past_key_values)
+                past_key_values = self.quanter.quant(past_key_values)
             # e = time.time()
             # print('----------',e-s)
             if decoding_simulation_length > 0:
