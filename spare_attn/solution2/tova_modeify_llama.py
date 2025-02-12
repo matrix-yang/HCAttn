f2b483d52cb1a4a749a5c2aa45c0c6d10967f0ae
ydq <matrix-yangdq@qq.com>
2025-02-12 15:03:30 +0800

    low mem tova

---
+++ spare_attn/solution2/tova_modeify_llama.py
@@ -0,0 +1,171 @@
+from typing import Optional, Tuple
+import os
+import torch
+from torch import nn
+import types
+from duo_attn.patch.tuple_kv_cache import enable_tuple_kv_cache, enable_tuple_kv_cache_for_llama
+import numpy as np
+import math
+import torch.nn.functional as F
+
+from transformers.models.llama.modeling_llama import (
+    LlamaForCausalLM,
+    LlamaModel,
+    repeat_kv,
+    apply_rotary_pos_emb,
+    CausalLMOutputWithPast,
+    List,
+    Union,
+    CrossEntropyLoss,
+    BaseModelOutputWithPast,
+)
+from flash_attn import flash_attn_func, flash_attn_with_kvcache
+from spare_attn.solution2.spare_decode import decode_token, decode_token_ori
+
+
+def enable_llama_approx_attention_eval(
+        model: LlamaForCausalLM, attn_sum, radio_bag
+):
+    enable_tuple_kv_cache_for_llama(model)
+    new_forward = warp_forward(attn_sum, radio_bag)
+    for idx, layer in enumerate(model.model.layers):
+        module = layer.self_attn
+        module.forward = types.MethodType(
+            new_forward, module
+        )
+
+
+def warp_forward(attn_sum, radio_bag):
+    def new_func(self,
+                 hidden_states: torch.Tensor,
+                 attention_mask: Optional[torch.Tensor] = None,
+                 position_ids: Optional[torch.LongTensor] = None,
+                 past_key_value: Optional[Tuple[torch.Tensor]] = None,
+                 output_attentions: bool = False,
+                 use_cache: bool = False,
+                 **kwargs, ):
+        return llama_approx_attention_forward(self,
+                                              hidden_states,
+                                              attention_mask,
+                                              position_ids,
+                                              past_key_value,
+                                              output_attentions,
+                                              use_cache,
+                                              attn_sum=attn_sum,
+                                              radio_bag=radio_bag,
+                                              **kwargs)
+
+    return new_func
+
+
+def llama_approx_attention_forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        attn_sum=0.5,
+        radio_bag=[],
+        **kwargs,
+) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+    bsz, q_len, _ = hidden_states.size()
+
+    # print(self.config._attn_implementation)
+
+    query_states = self.q_proj(hidden_states)
+    key_states = self.k_proj(hidden_states)
+    value_states = self.v_proj(hidden_states)
+
+    query_states = query_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+    key_states = key_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+    value_states = value_states.view(bsz, q_len, -1, self.head_dim).transpose(1, 2)
+
+    cos, sin = self.rotary_emb(value_states, position_ids)
+
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+    if past_key_value is not None:
+        # cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}  # Specific to RoPE models
+        # key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+        # print(past_key_value[0].shape,key_states.shape)
+        key_states = torch.cat([past_key_value[0], key_states], dim=2)
+        value_states = torch.cat([past_key_value[1], value_states], dim=2)
+
+    kv_seq_len = key_states.shape[2]
+
+    ## old forward
+    # attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+    # attn_output = torch.matmul(attn_weights, value_states)
+    #
+    # if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+    #     raise ValueError(
+    #         f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+    #         f" {attn_output.size()}"
+    #     )
+    #
+    # attn_output = attn_output.transpose(1, 2).contiguous()
+
+    # fast attn
+    attn_output = flash_attn_func(
+        query_states.transpose(1, 2),
+        key_states.transpose(1, 2),
+        value_states.transpose(1, 2),
+        causal=True,
+        dropout_p=0.0,
+    )
+
+    attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+
+    attn_output = self.o_proj(attn_output)
+
+    # 选cache
+    past_key_value = [key_states, value_states] if use_cache else None
+    key_states = repeat_kv(key_states, self.num_key_value_groups)
+    attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+    if q_len == kv_seq_len:
+        attention_mask = torch.full((kv_seq_len, kv_seq_len), fill_value=torch.finfo(query_states.dtype).min,
+                                    dtype=query_states.dtype, device=query_states.device)
+        attention_mask = torch.triu(attention_mask, diagonal=1)
+
+        attention_mask = attention_mask[None, None, :, :].expand(bsz, 1, -1, -1)
+        if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+            raise ValueError(
+                f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+            )
+        attn_weights = attn_weights + attention_mask
+
+    # upcast attention to fp32
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+
+    if q_len == kv_seq_len:
+        cache_size = int(key_states.shape[-2] * 0.5)
+        past_key_value = reduce_kv(past_key_value, attn_weights, cache_size)
+    else:
+        cache_size = key_states.shape[-2] - 1
+        past_key_value = reduce_kv(past_key_value, attn_weights, cache_size)
+    del attn_weights
+
+    if not output_attentions:
+        attn_weights = None
+
+    return attn_output, attn_weights, past_key_value
+
+
+def reduce_kv(past_key_value, attn_weights, cache_size):
+    bsz, _, _, num_keys = attn_weights.size()
+    _, num_kv_heads, _, _ = past_key_value[0].size()
+    print('k--', past_key_value[0].size())
+    # Utilize the average attention weights to select the top-k keys and values
+    mean_attn_weights = torch.mean(attn_weights[:, :, -1, :], dim=1).clone().detach()
+    print('mean_attn_weights--', mean_attn_weights.size(),cache_size)
+    vals, ind = torch.topk(mean_attn_weights, k=cache_size, dim=-1)
+    ind = torch.sort(ind).values  # stabelizes some things for some reason
+    expand_ind = ind.unsqueeze(1).unsqueeze(-1).expand(bsz, num_kv_heads, ind.size(-1),
+                                                       past_key_value[0].size(-1))
+
+    # Reduce the size of the cache to self.cache_size
+    past_key_value[0] = torch.gather(past_key_value[0], dim=2, index=expand_ind)
+    past_key_value[1] = torch.gather(past_key_value[1], dim=2, index=expand_ind)
+    return past_key_value
