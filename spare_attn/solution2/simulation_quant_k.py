c85b4078fac96297bc1ce7dd2b55d5e97e4e0584
ydq <matrix-yangdq@qq.com>
2025-02-13 15:19:06 +0800

    set dim=0 auto use dim

--- spare_attn/solution2/simulation_quant_k.py
+++ spare_attn/solution2/simulation_quant_k.py
@@ -2,13 +2,11 @@ import faiss
 import numpy as np
 import torch
 
-def load_index(path,dimension):
 
+def load_index(vectors, dimension):
     index = faiss.IndexFlatL2(dimension)
     res = faiss.StandardGpuResources()
     gpu_index = faiss.index_cpu_to_gpu(res, 0, index)
-
-    vectors = np.load(path)
     gpu_index.add(vectors)
     return gpu_index, vectors
 
@@ -27,7 +25,7 @@ def rebuid_norm_k(key_tensor, gpu_index, vectors):
 
 
 def rebuid_no_norm_k(key_tensor, gpu_index, vectors):
-    k=key_tensor.cpu().to(torch.float).numpy()
+    k = key_tensor.cpu().to(torch.float).numpy()
     k_size = k.shape
     # k_norms=np.linalg.norm(k, ord=2, axis=-1,keepdims=True)
     # normlize_k=k/k_norms
@@ -42,17 +40,18 @@ def rebuid_no_norm_k(key_tensor, gpu_index, vectors):
     # rebuild_kk=rebuild_kk*k_norms
     return rebuild_kk
 
-def batch_rebuid_no_norm_k(key_tensor, gpu_index, vectors,bits):
-    k=key_tensor.cpu().to(torch.float).numpy()
+
+def batch_rebuid_no_norm_k(key_tensor, gpu_index, vectors, bits):
+    k = key_tensor.cpu().to(torch.float).numpy()
     k_size = k.shape
     # k_norms=np.linalg.norm(k, ord=2, axis=-1,keepdims=True)
     # normlize_k=k/k_norms
     low_k = k.reshape(-1, bits)
-    batch=32*32*32*1024
-    all_indices=np.zeros(low_k.shape[0],dtype=np.int32)
-    for i in range(0,low_k.shape[0],batch):
-        distances, indices = gpu_index.search(low_k[i:i+batch], 1)
-        all_indices[i:i+batch]=indices.reshape(-1)
+    batch = 32 * 32 * 32 * 1024
+    all_indices = np.zeros(low_k.shape[0], dtype=np.int32)
+    for i in range(0, low_k.shape[0], batch):
+        distances, indices = gpu_index.search(low_k[i:i + batch], 1)
+        all_indices[i:i + batch] = indices.reshape(-1)
     rebuild_kk = vectors[all_indices]
     rebuild_kk = rebuild_kk.reshape(k_size)
 
@@ -61,20 +60,23 @@ def batch_rebuid_no_norm_k(key_tensor, gpu_index, vectors,bits):
 
 
 class Quanter():
-    def __init__(self,p,dims=4):
-        #p = '/nfs/hw-data/ms/FM/ydq/notebook/duo_attn/no_norm_4bits_8196.npy'
-        self.dims=dims
-        self.gpu_index, self.vectors = load_index(p,dims)
+    def __init__(self, p, dims=0):
+        # p = '/nfs/hw-data/ms/FM/ydq/notebook/duo_attn/no_norm_4bits_8196.npy'
+        vectors = np.load(p)
+        if dims == 0:
+            self.dims = vectors.shape[-1]
+        print(f'use quant {p} dims {self.dims}')
+        self.gpu_index, self.vectors = load_index(vectors, self.dims)
 
-    def quant(self,past_key_values):
-        #print('----------do_quant')
+    def quant(self, past_key_values):
+        # print('----------do_quant')
         new_past_key_values = []
         # bsz head len 128
         k = torch.cat([past_key_values[i][0] for i in range(32)])
         v = torch.cat([past_key_values[i][1] for i in range(32)])
-        #print('k.shape',k.shape)
-        #rbk = rebuid_no_norm_k(k, self.gpu_index, self.vectors)
-        rbk = batch_rebuid_no_norm_k(k, self.gpu_index, self.vectors,self.dims)
+        # print('k.shape',k.shape)
+        # rbk = rebuid_no_norm_k(k, self.gpu_index, self.vectors)
+        rbk = batch_rebuid_no_norm_k(k, self.gpu_index, self.vectors, self.dims)
         del k
         torch.cuda.empty_cache()
         selected_k = torch.from_numpy(rbk).to(v.dtype).cuda()
@@ -83,8 +85,9 @@ class Quanter():
         new_past_key_values = [(selected_k[i].unsqueeze(0), selected_v[i].unsqueeze(0)) for i in
                                range(selected_k.shape[0])]
         # print(f'K shape {k.shape} cal sim {b - a} cal cmp {c - b} replace{d - c} all {d - a}')
-        #print('----------end_quant')
+        # print('----------end_quant')
         return new_past_key_values
+
 
 class NormQuanter():
     def __init__(self):
